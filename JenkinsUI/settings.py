b626e5cff7f1500286d0c91aa02e7a2e96e7c6d7
liaogx <liagox@liaogx.com>
2019-07-10 10:57:08 +0800

    Initial commit

--- JenkinsUI/settings.py
+++ JenkinsUI/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -112,3 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
---
+++ apps/data/models.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# __author__ = 'liao gao xiang'
+
+from django.db import models
